1c9adcdd7e0fdf4e02553000d5aac9423a9154ff
Mercy Ma <mercyblitz@gmail.com>
2025-02-25 18:15:34 +0800

    Create Plugins.java

---
+++ microsphere-mybatis-core/src/main/java/io/microsphere/mybatis/plugin/Plugins.java
@@ -0,0 +1,113 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.mybatis.plugin;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.executor.parameter.ParameterHandler;
+import org.apache.ibatis.executor.resultset.ResultSetHandler;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.PluginException;
+import org.apache.ibatis.plugin.Signature;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static io.microsphere.collection.ListUtils.ofList;
+import static io.microsphere.text.FormatUtils.format;
+import static org.apache.ibatis.util.MapUtil.computeIfAbsent;
+
+/**
+ * The utilities class of {@link Plugin}
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see Plugin
+ * @see Intercepts
+ * @see Signature
+ * @since 1.0.0
+ */
+public abstract class Plugins {
+
+    /**
+     * The valid target classes for {@link Interceptor}
+     */
+    public static final List<Class<?>> TARGET_CLASSES = ofList(Executor.class, ParameterHandler.class,
+            ResultSetHandler.class, StatementHandler.class);
+
+    /**
+     * The size of {@link #TARGET_CLASSES}
+     */
+    public static final int TARGET_CLASSES_SIZE = TARGET_CLASSES.size();
+
+    /**
+     * Get the {@link Signature} map from the specified {@link Interceptor} that was annotated {@link Intercepts}
+     *
+     * @param interceptor the {@link Interceptor} that was annotated {@link Intercepts}
+     * @return
+     * @throws PluginException if No {@link Intercepts @Intercepts} annotation was found in interceptor or
+     *                         no {@link Signature @Signature} was found in the {@link Intercepts @Intercepts}
+     * @see Plugin#getSignatureMap(Interceptor)
+     */
+    public static Map<Class<?>, Set<Method>> getSignatureMap(Interceptor interceptor) throws PluginException {
+        return getSignatureMap(interceptor.getClass());
+    }
+
+    /**
+     * Get the {@link Signature} map from the specified {@link Interceptor} that was annotated {@link Intercepts}
+     *
+     * @param interceptorClass the {@link Interceptor} that was annotated {@link Intercepts}
+     * @return
+     * @throws PluginException if No {@link Intercepts @Intercepts} annotation was found in interceptorClass or
+     *                         no {@link Signature @Signature} was found in the {@link Intercepts @Intercepts}
+     * @see Plugin#getSignatureMap(Interceptor)
+     */
+    public static Map<Class<?>, Set<Method>> getSignatureMap(Class<? extends Interceptor> interceptorClass) throws PluginException {
+        Intercepts interceptsAnnotation = interceptorClass.getAnnotation(Intercepts.class);
+        // issue #251
+        if (interceptsAnnotation == null) {
+            throw newPluginException("No @Intercepts annotation was found in interceptorClass : {}", interceptorClass.getName());
+        }
+        Signature[] sigs = interceptsAnnotation.value();
+        // @Intercepts usually specifies one or two targets, so HashMap can be optimized
+        Map<Class<?>, Set<Method>> signatureMap = new HashMap<>(TARGET_CLASSES_SIZE / 2, 1.0f);
+        for (Signature sig : sigs) {
+            Class<?> targetType = sig.type();
+            if (!TARGET_CLASSES.contains(targetType)) {
+                throw newPluginException("The @Intercepts#type() =  {} must be one of the target classes : {}", targetType.getName(), TARGET_CLASSES);
+            }
+            Set<Method> methods = computeIfAbsent(signatureMap, targetType, k -> new HashSet<>());
+            String methodName = sig.method();
+            try {
+                Method method = targetType.getMethod(methodName, sig.args());
+                methods.add(method);
+            } catch (NoSuchMethodException e) {
+                throw new PluginException(format("Could not find method on {} named {}", targetType, methodName), e);
+            }
+        }
+        return signatureMap;
+    }
+
+    protected static PluginException newPluginException(String messagePattern, Object... args) {
+        return new PluginException(format(messagePattern, args));
+    }
+}
